f834384c03864694bddffb14a35dd8e21fd675d2
Gott50 <xxGott50xx@gmail.com>
2019-04-13 00:23:15 +0200

    add POST to /mail/wrong_login_data/

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+
+from flask import request
 from flask_mail import Message
 
 from config import setup_mail
@@ -14,6 +17,20 @@ def index():
                   subject="test"
                   )
     mail.send(msg)
+
+
+@app.route("/mail/wrong_login_data/", methods=['POST'])
+def wrong_login_data():
+    try:
+        data = json.loads(request.data)
+        email = data["email"]
+        username = data["username"]
+
+        return "%s: %s" % (email, username)
+
+
+    except KeyError as e
+        return 600 # TODO research right status code
 
 
 if __name__ == '__main__':
