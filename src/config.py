3635a34f0b19c69cf7e146f55de4ab132e6d78ea
Gott50 <xxGott50xx@gmail.com>
2019-04-16 00:06:59 +0200

    reduce Flask BaseConfig

--- src/config.py
+++ src/config.py
@@ -2,25 +2,7 @@ import os
 
 
 class BaseConfig(object):
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ['SECRET_KEY']
     DEBUG = os.environ['DEBUG']
-    DB_NAME = os.environ['DB_NAME']
-    DB_USER = os.environ['DB_USER']
-    DB_PASS = os.environ['DB_PASS']
-    DB_SERVICE = os.environ['DB_SERVICE']
-    DB_PORT = os.environ['DB_PORT']
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
-        DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
-    )
-    SECURITY_PASSWORD_SALT = os.environ['SECURITY_PASSWORD_SALT']
-    # SECURITY_CONFIRMABLE = True   # /confirm
-    # SECURITY_REGISTERABLE = True  # /register
-    # SECURITY_CHANGEABLE = True    # /change
-    SECURITY_RECOVERABLE = True  # /reset
-    SECURITY_RESET_URL = '/api/reset'
-
-    SECURITY_EMAIL_SENDER = os.environ['SECURITY_EMAIL_SENDER']
 
 # At top of file
 from flask_mail import Mail
@@ -28,6 +10,7 @@ from flask_mail import Mail
 
 # After 'Create app'
 def setup_mail(app):
+    app.config['MAIL_DEFAULT_SENDER'] = os.environ['SECURITY_EMAIL_SENDER']
     app.config['MAIL_SERVER'] = os.environ['MAIL_SERVER']
     app.config['MAIL_PORT'] = os.environ['MAIL_PORT']
     app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL', True)
